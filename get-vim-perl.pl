e677c144859b833520000a9c66aafd876f53144f
Spencer Myers <dsmyers@cent2.linux.home>
2018-10-28 11:09:34 -0600

    Had to change from wget to curl

--- get-vim-perl.pl
+++ get-vim-perl.pl
@@ -5,22 +5,24 @@ use warnings;
 use utf8;
 
 
-my $home = $ENV{HOME};
+#my $home = $ENV{HOME};
+my $user='dsmyers';
+my $home="/home/$user";
+
 chomp(my $date = `date +%s`);
 my $wget_out = 'vim_perl.html';
 my $ide_url = "https://www.vim.org/scripts/script.php?script_id=556";
 
 my $vim_dir = "$home/.vim_$date";
+print "VIMDIR $vim_dir\n";
 if (-d "$home/.vim") {
   system("mv $home/.vim $home/.vim_orig_$date");
 }
 mkdir $vim_dir;
 system("cd $home && ln -s .vim_$date .vim");
-
 if (not -f "./$wget_out") {
-  system("wget -O $wget_out $ide_url") if not -f "./$wget_out";
+  system("curl -o $wget_out $ide_url") if not -f "./$wget_out";
 }
-
 my $index;
 open my $fh, '<', $wget_out or die "Could not open file '$wget_out' $!";
 while (<$fh>) {
@@ -33,10 +35,11 @@ while (<$fh>) {
 
 unlink "./$wget_out";
 
-system("wget -O perl-support.zip https://www.vim.org/scripts/download_script.php?src_id=$index") if not -f "./perl-support.zip";
+system("curl -o perl-support.zip https://www.vim.org/scripts/download_script.php?src_id=$index") if not -f "./perl-support.zip";
+print "mv ./perl-support.zip $vim_dir\n";
 system("mv ./perl-support.zip $vim_dir");
 chdir $vim_dir;
-system("unzip ./perl-support.zip");
+system("unzip $vim_dir/perl-support.zip");
 unlink "./perl-support.zip";
 
 
--- setup_env.pl
+++ setup_env.pl
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-myhome='/home/myerds'
-myuser='myerds'
-cp ./google.repo /etc/yum.repos.d/
-yum install -y wget perl epel-release vim ansible python-pip
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied"
+    echo "Please choose server or ws"
+    exit 1
+fi
+
+#myuser=`whoami`
+myuser='dsmyers'
+myhome="/home/$myuser"
+yum install -y curl wget perl epel-release vim ansible python-pip unzip
 yum -y update
 
 cp ./.vimrc $myhome
@@ -16,7 +23,11 @@ chown -R $myuser: $myhome
 
 cp ./comments.templates $myhome/.vim/perl-support/templates/comments.templates
 cp ./idioms.templates $myhome/.vim/perl-support/templates/idioms.templates
-cp ./gtk.css $myhome/.config/gtk-3.0
 
+
+if [ $1 = 'ws' ]; then
+    cp ./google.repo /etc/yum.repos.d/
+    cp ./gtk.css $myhome/.config/gtk-3.0
+fi
 
 
